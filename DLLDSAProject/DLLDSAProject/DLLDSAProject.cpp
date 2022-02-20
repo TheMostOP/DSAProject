e36238ce072ad5f2c961292d715893b69a1d8f56
Connor Reif <cjr8321@rit.edu>
2022-02-20 13:15:56 -0500

    Revert "Progress (test commit+push)"
    This reverts commit b1dd73e08c49ebdfc459d822dadf01e082a8a3f5.

--- DLLDSAProject/DLLDSAProject/DLLDSAProject.cpp
+++ DLLDSAProject/DLLDSAProject/DLLDSAProject.cpp
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 // DLLDSAProject.cpp : Defines the exported functions for the DLL.
 //
 
@@ -21,46 +20,3 @@ CDLLDSAProject::CDLLDSAProject()
 {
     return;
 }
-=======
-// DLLDSAProject.cpp : Defines the exported functions for the DLL.
-//
-
-#include "pch.h"
-#include "framework.h"
-#include "DLLDSAProject.h"
-
-
-// This is an example of an exported variable
-DLLDSAPROJECT_API int nDLLDSAProject=0;
-
-// This is an example of an exported function.
-DLLDSAPROJECT_API int fnDLLDSAProject(void)
-{
-    return 0;
-}
-
-// This is the constructor of a class that has been exported.
-CDLLDSAProject::CDLLDSAProject()
-{
-    return;
-}
-
-
-// Start of Connor's functions
-DLLDSAPROJECT_API const char* names = "Luke and Connor";
-DLLDSAPROJECT_API char* GetTeam()
-{
-    return (char*)names;
-}
-
-DLLDSAPROJECT_API const int** mazeData = nullptr;
-DLLDSAPROJECT_API int mazeWidth = 0;
-DLLDSAPROJECT_API int mazeHeight = 0;
-
-DLLDSAPROJECT_API void SetMaze(const int** data, int width, int height)
-{
-    mazeData = data;
-    mazeWidth = width;
-    mazeHeight = height;
-}
->>>>>>> Stashed changes
