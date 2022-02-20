0b919ecbd26ef146992608fdf71eed4c1017834e
Connor Reif <cjr8321@rit.edu>
2022-02-20 15:41:55 -0500

    Unit test for the Get/SetMaze functions

--- DLLDSAProject/UnitTest1/UnitTest1.cpp
+++ DLLDSAProject/UnitTest1/UnitTest1.cpp
@@ -16,5 +16,33 @@ namespace UnitTest1
 			char* ret = GetTeam();
 			Assert::AreNotEqual(test, ret);
 		}
+
+		TEST_METHOD(TestMazeDataFuncs)
+		{
+			const int width = 10;
+			const int height = 5;
+			int testData[width][height] = {
+				{ 11, 12, 13, 14, 15 },
+				{ 21, 22, 23, 24, 25 },
+				{ 31, 32, 33, 34, 35 },
+				{ 41, 42, 43, 44, 45 },
+				{ 51, 52, 53, 54, 55 },
+				{ 61, 62, 63, 64, 65 },
+				{ 71, 72, 73, 74, 75 },
+				{ 81, 82, 83, 84, 85 },
+				{ 91, 92, 93, 94, 95 },
+				{ 01, 02, 03, 04, 05 }
+			};
+			int* testDataPtr = *testData;
+			SetMaze(&testDataPtr, width, height);
+
+			int* retWidth = new int();
+			int* retHeight = new int();
+			int* retDataPtr = *(GetMaze(*retWidth, *retHeight));
+
+			Assert::AreEqual(width, *retWidth);
+			Assert::AreEqual(height, *retHeight);
+			Assert::AreEqual(testData[0][2], (&retDataPtr)[0][2]);
+		}
 	};
 }
